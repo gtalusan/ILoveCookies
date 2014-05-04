2ab28da688edeb79d661a87c2b81c20134e441c1
George Talusan <george.talusan@gmail.com>
2014-05-03 22:16:13 -0400

    move blacklist into its own .js

---
+++ ILoveCookies.safariextension/blacklist.js
@@ -0,0 +1,5 @@
+var blacklist = {
+	"thestar.com": [ "syncwall-history=; path=/;", "syncwall-sessionid=; path=/;" ],
+	"www.torontosun.com": [ "pw_meter_tos=; path=/; domain=.torontosun.com;" ],
+	"theglobeandmail.com": [ "METER_COOKIE=; path=/; domain=.theglobeandmail.com;" ],
+};
